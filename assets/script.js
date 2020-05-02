7eb173f7ed04b99f66d9999f839392e07b65a79e
Kaeneth Dayao <kendayao@gmail.com>
2020-05-02 12:49:25 -0700

    header added and set up ajax response

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,16 @@
-var queryURL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}";
+
+var city = "Los Angeles"
 var apiKey = "bb06c0b8789f5256fcbbe492b33425e3";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
 
 
+$.ajax({
+
+url: queryURL,
+method: "GET"
+
+})
+.then(function(response){
+    console.log(queryURL)
+console.log (response)
+});
