bf2d16fb98781159d98ec8ff8d2600bed48894e4
Kaeneth Dayao <kendayao@gmail.com>
2020-05-02 19:51:29 -0700

    added search input field

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,11 @@
 var city = "Los Angeles"
 var apiKey = "bb06c0b8789f5256fcbbe492b33425e3";
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
-
+$("#searchbutton").on("click", function(event){
+   
+    event.preventDefault();
+    console.log($("input").val())
+});
 
 $.ajax({
 
